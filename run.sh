05d6c3f4190e36d7bfdec7ea740b62f2d2218c72
diegomoca <136346913+diegomoca@users.noreply.github.com>
2025-08-02 10:10:59 +0200

    superCollider server start before video

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ rm -f video_started.flag
 mkfifo audio.wav
 
 # inciciar script superCollider
-# sclang audio-generator/script.scd &
+sclang audio-generator/script.scd &
 
 
 while [ ! -f audio_ready.flag ]; do
@@ -28,7 +28,7 @@ done
 # generacion de video y conversion de vfr a cfr
 video-generator/build/main | ffmpeg \
 -f rawvideo -pix_fmt rgba -video_size 400x300 -use_wallclock_as_timestamps 1 -i - \
--f s16le -ar 44100 -ac 2 -i audio.wav \
+-f s16le -ar 44100 -ac 2  -use_wallclock_as_timestamps 1 -i audio.wav \
 -vf "vflip" \
 -r 30 -c:v libx264 -c:a aac -pix_fmt yuv420p -shortest \
 output.mp4
