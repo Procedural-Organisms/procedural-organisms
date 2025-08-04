a98add8e7a7d2dca46276d59c3c2f1318e0b86be
diegomoca <morales.castillo.diego@gmail.com>
2025-08-04 20:28:37 +0000

    migrated to more powerful server and optimized program

--- run.sh
+++ run.sh
@@ -5,9 +5,11 @@
 trap 'echo "Cleaning up..."; kill 0; rm -f audio.wav output.mp4 audio_ready.flag video_started.flag; exit' SIGINT
 
 # localizacion de pipes
+pipes_dir="temp/pipes"
 audio_pipe="temp/pipes/audio_pipe"
 video_pipe="temp/pipes/video_pipe"
 # localizacion de flags
+flags_dir="temp/flags"
 audio_ready="temp/flags/audio_ready.flag"
 ffmpeg_started="temp/flags/ffmpeg_started.flag"
 video_started="temp/flags/video_started.flag"
@@ -28,15 +30,27 @@ rm -f "$audio_ready"
 rm -f "$ffmpeg_started"
 rm -f "$video_started"
 
+# crear directorios temporales
+if [ ! -d "$pipes_dir" ]; then
+    mkdir -p "$pipes_dir"
+fi
+if [ ! -d "$flags_dir" ]; then
+    mkdir -p "$flags_dir"
+fi
+
 # crear pipes
 mkfifo "$audio_pipe"
 mkfifo "$video_pipe"
 
-jackd -d alsa -r 44100 -p 4096 -n 3 -S &
+# Iniciar servidor JACK
+jackd -d dummy -r 44100 -p 512 &
+JACK_PID=$!
+sleep 2
 
 # inciciar script superCollider
 # que inicia servidor y espera a que comience la generacion de video
 sclang "$audio_generator" &
+AUDIO_GENERATOR_PID=$!
 
 while [ ! -f "$audio_ready" ]; do
     sleep 0.1
@@ -45,6 +59,7 @@ done
 # abrir generador de video en el background
 # que espera a que ffmpeg comience para generar video
 "$video_generator" > "$video_pipe" &
+VIDEO_GENERATOR_PID=$!
 
 # esperar en el background a que se abra ffmpeg y crear flag
 {
@@ -56,10 +71,13 @@ touch "$ffmpeg_started"
 
 # generacion de video y conversion de vfr a cfr
 ffmpeg \
--thread_queue_size 4096 -f rawvideo -pix_fmt rgba -video_size 400x300 -use_wallclock_as_timestamps 1 -i "$video_pipe" \
--thread_queue_size 4096 -f s16le -ar 44100 -ac 2 -i "$audio_pipe" \
+-thread_queue_size 512 -f rawvideo -pix_fmt rgba -video_size 400x300 -use_wallclock_as_timestamps 1 -i "$video_pipe" \
+-thread_queue_size 512 -f s16le -ar 44100 -ac 2 -i "$audio_pipe" \
 -vf "vflip" \
 -r 30 -f flv -c:v libx264 -pix_fmt yuv420p -b:v 400k -c:a aac -ar 44100 -b:a 128k "$output"
+
+kill $JACK_PID $AUDIO_GENERATOR_PID $VIDEO_GENERATOR_PID
+wait $JACK_PID $AUDIO_GENERATOR_PID $VIDEO_GENERATOR_PID
 
 # eliminar pipes
 rm -f "$audio_pipe"
@@ -68,3 +86,4 @@ rm -f "$video_pipe"
 rm -f "$audio_ready"
 rm -f "$ffmpeg_started"
 rm -f "$video_started"
+
