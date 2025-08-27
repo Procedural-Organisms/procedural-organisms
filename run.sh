7cbbedbb818868b8faae1a713b00ca941e04430b
diegomoca <morales.castillo.diego@gmail.com>
2025-08-27 23:50:22 +0000

    increased sample rate to 48khz

--- run.sh
+++ run.sh
@@ -51,7 +51,7 @@ mkfifo "$video_pipe"
 
 # Iniciar servidor JACK solo en Linux
 if [[ "$OSTYPE" != "darwin"* ]]; then
-    jackd -d dummy -r 44100 -p 512 &
+    jackd -d dummy -r 48000 -p 512 &
     sleep 2
 fi
 
@@ -72,6 +72,6 @@ touch "$ffmpeg_started"
 # generacion de video y conversion de vfr a cfr
 ffmpeg \
 -thread_queue_size 512 -f rawvideo -pix_fmt rgba -video_size 400x300 -use_wallclock_as_timestamps 1 -i "$video_pipe" \
--thread_queue_size 512 -f s16le -ar 44100 -ac 2 -i "$audio_pipe" \
+-thread_queue_size 512 -f s16le -ar 48000 -ac 2 -i "$audio_pipe" \
 -vf "vflip" \
--r 30 -f flv -c:v libx264 -pix_fmt yuv420p -b:v 400k -c:a aac -ar 44100 -b:a 128k "$output"
+-r 30 -f flv -c:v libx264 -pix_fmt yuv420p -b:v 400k -c:a aac -ar 48000 -b:a 128k "$output"
