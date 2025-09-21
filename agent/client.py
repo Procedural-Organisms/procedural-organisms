390883bcc7752c165b261c44d6290b03b9714106
diegomoca <diegomoca@Mac.fritz.box>
2025-09-21 19:52:11 +0200

    configure superCollider main file to open other .scd files

--- agent/client.py
+++ agent/client.py
@@ -2,7 +2,7 @@ from osc4py3.as_eventloop import *
 
 # definir funcion que toma como argumentos una direccion ip y dos puertos
 # y crea 2 cientes de distintos nombres con distintos puertos para mandar mensajes osc
-def client_startup(address: str = '127.0.0.1', port1: int = 57120):
+def client_startup(address: str = '127.0.0.1', port1: int = 13933):
     osc_startup()
     osc_udp_client(address, port1, 'scClient')
     print("OSC client started!")
